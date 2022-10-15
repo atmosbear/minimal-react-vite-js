3f11e633328baaa66411ff0c19c8e931958d5924
atmosbear <mondegreen84@protonmail.com>
2022-10-15 12:41:19 -0700

    Added blue banner with tailwind!

--- src/Component.jsx
+++ src/Component.jsx
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function Component() {
 	return (
-		<div>I am a minimal component.</div>
+		<div className="bg-blue-500">I am a minimal component.</div>
 	)
 }
